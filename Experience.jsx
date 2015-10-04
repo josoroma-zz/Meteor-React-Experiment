482dadff4952894ddc9b55315d7c52e6ee272f46
josoroma <pablo@josoroma.com>
2015-10-04 07:54:13 -0600

    Update and delete items

--- Experience.jsx
+++ Experience.jsx
@@ -3,11 +3,41 @@ Experience = React.createClass({
   propTypes: {
     // This component gets the experience to display through a React prop.
     // We can use propTypes to indicate it is required.
-    experience: React.PropTypes.object.isRequired
+    experience: React.PropTypes.object.isRequired,
   },
+
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Experiences.update(this.props.experience._id, {
+      $set: {
+        checked: !this.props.experience.checked,
+      },
+    });
+  },
+
+  deleteThisExperience() {
+    Experiences.remove(this.props.experience._id);
+  },
+
   render() {
+    // Give experiences a different className when they are checked off,
+    // so that we can style them nicely in CSS
+    const experienceClassName = this.props.experience.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.experience.title}</li>
+      <li className={experienceClassName}>
+        <button className="delete" onClick={this.deleteThisExperience} >
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly={true}
+          checked={this.props.experience.checked}
+          onClick={this.toggleChecked} />
+
+        <span className="title">{this.props.experience.title}</span>
+      </li>
     );
   }
 });
